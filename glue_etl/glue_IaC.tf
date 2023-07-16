d8132d2f9c6645f4aa230f6162289d6ea513dabf
demeritiusg <demeritiusgriffin@gmail.com>
2023-07-16 14:09:43 -0700

    added new files

--- glue_etl/glue_IaC.tf
+++ glue_etl/glue_IaC.tf
@@ -21,6 +21,13 @@ resource "aws_glue_catalog_table" "etl_catalog_table_prod" {
     database_name = "etl_catalog_database_prod"
 }
 
+resource "aws_glue_crawler" "etl_catalog_crawler_prod" {
+    database_name = aws_glue_catalog_database.etl_catalog_database_prod
+    name = "etl_catalog_crawler_prod"
+    role = aws_iam_role.catalog_crawler_prod 
+  
+}
+
 resource "aws_glue_classifier" "etl_csv_prod_classifier" {
     name = "project1_csv_classifier"
 
@@ -31,6 +38,11 @@ resource "aws_glue_classifier" "etl_csv_prod_classifier" {
     }
 }
 
-resource "aws_glue_job" "etl_prod_job" {
-  
+resource "aws_glue_job" "nyc_trip_data_job" {
+    name = "nyc_trip_data"
+    role_arn = aws_iam_role.nyc_trip_role
+
+    command {
+      script_location = "${var.s3_bucket}/nyc_tripdata_etl.py"
+    }
 }
