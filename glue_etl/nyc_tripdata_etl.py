62c630a8c977b019ae3712fa90b2b376bf8e3731
demeritiusg <demeritiusgriffin@gmail.com>
2024-03-25 06:45:39 -0700

    glue edits

--- glue_etl/nyc_tripdata_etl.py
+++ glue_etl/nyc_tripdata_etl.py
@@ -23,7 +23,7 @@ destination_bucket = ''
 source_path = f's3://{source_bucket}/{obj_key}'
 output_path = 's3://' + destination_bucket + '/' + obj_key.rsplit(".",1)[0]
 
-# read data from s3
+# read data from s3 --->  bronze
 read_data = gc.create_dynamic_frame.from_options(
 	connection_type='s3',
 	connection_options={'path': [source_path]},
@@ -39,12 +39,16 @@ datasource = spark_df.dropna(thresh=4)
 
 #replcaing missing values
 pickup_clean_dataDF = datasource.fillna(value='n/a_ride', subset='tpep_pickup_hour')
+dropoff_clean_dataDF = datasource.fillna(value='n/a_ride', subset='tpep_dropoff_hour')
 
+
+#parsing dates --->  silver
 pickup_clean_dataDF.select(hour('pickup_datetime').alias('tpep_pickup_hour'), year('pickup_datetime').alias('tpep_pickup_year')/
                     month('pickup_datetime').alias('tpep_pickup_hour'), weekofyear('pickup_datetime').alias('tpep_pickup_weekday'))
 
 
-
+dropoff_clean_dataDF.select(hour('dropoff_datetime').alias('tpep_dropoff_hour'), year('dropoff_datetime').alias('tpep_dropoff_year')/
+                    month('dropoff_datetime').alias('tpep_dropoff_month'), weekofyear('dropoff_datetime').alias('tpep_dropoff_weekday'))
 
 # finish job
 job.commit()
